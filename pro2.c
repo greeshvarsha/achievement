4e9c6f7395dce3a59996492851dae1d721b792a0
greeshvarsha <32356717+greeshvarsha@users.noreply.github.com>
2017-09-28 14:30:06 +0530

    Create pro2.c

---
+++ pro2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int num;
+scanf("%d",&num);
+if(num<0)
+{
+printf("%d is negative number",num);
+}
+else
+{
+printf("%d is not a negative number",num);
+}
+getch();
+}
